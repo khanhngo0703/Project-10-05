435283872f798d0735be7cc0b66408bcec138209
khanhngo0703 <khanhcr2807@gmail.com>
2023-05-10 13:17:23 +0700

    first commit

---
+++ shared/_footer.php
@@ -0,0 +1,165 @@
+<section class="discount spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 p-0">
+                <div class="discount_pic">
+                    <img src="images/discount.jpg" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-lg-6 p-0">
+                <div class="discount_text">
+                    <div class="discount_text_title">
+                        <span>Discount</span>
+                        <h2>Summer 2023</h2>
+                        <h5><span>Sale</span> 60% OFF</h5>
+                    </div>
+                    <a href="#">Buy now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- <section class="instagram">
+    <div class="container-fluid">
+        <div class="row m-0">
+            <div class="col-lg-3 p-0">
+                <div class="instagram_item">
+                    <img src="images/1.jpg">
+                    <div class="instagram_text">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 p-0">
+                <div class="instagram_item">
+                    <img src="images/2.jpg">
+                    <div class="instagram_text">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 p-0">
+                <div class="instagram_item">
+                    <img src="images/3.jpg">
+                    <div class="instagram_text">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 p-0">
+                <div class="instagram_item">
+                    <img src="images/1.jpg">
+                    <div class="instagram_text">
+                        <i class="fa-brands fa-instagram"></i>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </div>
+</section> -->
+<div class="slider_form">
+    <div class="slider">
+        <div class="slides">
+            <input type="radio" name="radio-btn" id="radio1">
+            <input type="radio" name="radio-btn" id="radio2">
+            <input type="radio" name="radio-btn" id="radio3">
+
+            <div class="slide first">
+                <img src="/images/1.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="/images/2.jpg" alt="">
+            </div>
+            <div class="slide">
+                <img src="/images/3.jpg" alt="">
+            </div>
+
+            <div class="navigation-auto">
+                <div class="auto-btn1"></div>
+                <div class="auto-btn2"></div>
+                <div class="auto-btn3"></div>
+            </div>
+
+        </div>
+
+        <div class="navigation-manual">
+            <label for="radio1" class="manual-btn"></label>
+            <label for="radio2" class="manual-btn"></label>
+            <label for="radio3" class="manual-btn"></label>
+        </div>
+
+    </div>
+</div>
+
+
+<section class="footer spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="footer_about">
+                    <div class="footer_logo">
+                        <a href="index.html">Fashion Shop<span>.</span></a>
+                    </div>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry.</p>
+                    <div class="footer_social">
+                        <a href="#">
+                            <i class="fa-brands fa-facebook-f"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3">
+                <div class="footer_widget">
+                    <h6>Quick Links</h6>
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Blogs</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3">
+                <div class="footer_widget">
+                    <h6>Accounts</h6>
+                    <ul>
+                        <li><a href="#">My Account</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Order Tracking</a></li>
+                        <li><a href="#">Checkout</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-3">
+                <div class="footer_widget">
+                    <h6>Support</h6>
+                    <ul>
+                        <li><a href="#">Frequently Asked Questions</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Report a Payment Issue</a></li>
+                        <li><a href="#">24/7 Support</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
